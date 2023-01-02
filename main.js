64ed0f3469c883716bf1cee495d78d1f1cc06a36
Bart Bieszczad <bbart318@gmail.com>
2023-01-02 00:41:46 -0600

    Fix mini logo display

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const burgerBtn = document.querySelector(".fa-bars");
 const header = document.querySelector("header")
 const content = document.querySelector(".content-main");
 const sidebar = document.querySelector("aside");
+const miniLogo = document.querySelector(".min-logo")
 
 
 
 burgerBtn.addEventListener("click", () => {
     
     header.classList.toggle("nav-to-side");
+    miniLogo.classList.toggle("hide-logo");
     content.classList.toggle("darken-content");
     sidebar.classList.toggle("side-open");
     sidebar.classList.toggle("side-hidden");
+
 })
 
 content.addEventListener("click", (e) => {
